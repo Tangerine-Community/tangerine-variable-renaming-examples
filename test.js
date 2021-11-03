904db55b4fbd7bbfc15033df8322e16acc874713
R.J. (Steinert) Corwin <rj@rjsteinert.com>
2021-11-03 13:04:11 -0400

    Add tests to test.js

--- test.js
+++ test.js
@@ -1,5 +1,17 @@
 const transformer = require('./transformer.js')
+
+function test(name, pass) {
+  if (pass) {
+    console.log(`PASS: ${name}`)
+  } else {
+    console.error(`FAIL: ${name}`)
+  }
+}
+
 const responseZ = require('./stubs/response-z.json')
+const transformedResponseZ = transformer(responseZ)
+test('Variable map should rename zab to zebra in responseZ', transformedResponseZ.items[1].inputs[0].name === 'zebra') 
+
 const responseX = require('./stubs/response-x.json') 
-console.log(JSON.stringify(transformer(responseZ), null, 2))
-console.log(JSON.stringify(transformer(responseX), null, 2))
+const transformedResponseX = transformer(responseX)
+test('Variable map should rename zab to zebra in responseX', transformedResponseX.items[0].inputs[0].name === 'zebra') 
